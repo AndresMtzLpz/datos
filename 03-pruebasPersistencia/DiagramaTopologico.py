048ed68781426d7b5c93954049b8c5dbc64ffc73
Andres Martinez <andres@servidor>
2025-10-31 10:11:28 -0500

    Grafo9

--- 03-pruebasPersistencia/DiagramaTopologico.py
+++ 03-pruebasPersistencia/DiagramaTopologico.py
@@ -132,7 +132,7 @@ if __name__ == "__main__":
         graph = mapper.map(
             lens,
             X,
-            cover=Cover(n_cubes=23, perc_overlap=0.5),
+            cover=Cover(n_cubes=50, perc_overlap=0.01),
             clusterer=DBSCAN(eps=3, min_samples=80)
         )
 
