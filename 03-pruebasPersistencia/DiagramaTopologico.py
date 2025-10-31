91f3633e9f8069b4e9d5c17e2b57c8475e301a3c
Andres Martinez <andres@servidor>
2025-10-31 09:23:16 -0500

    Grafo5

--- 03-pruebasPersistencia/DiagramaTopologico.py
+++ 03-pruebasPersistencia/DiagramaTopologico.py
@@ -133,7 +133,7 @@ if __name__ == "__main__":
             lens,
             X,
             cover=Cover(n_cubes=23, perc_overlap=0.1),
-            clusterer=DBSCAN(eps=0.5, min_samples=5)
+            clusterer=DBSCAN(eps=0.5, min_samples=50)
         )
 
         # Paso 6: Visualizar el grafo con detalles por prefijo
